e8127e8a7bc86f29dc68ae3bcb1f7a30489fe750
Nimesh Dhruve <nimeshdhruve@gmail.com>
2018-12-08 22:06:11 -0500

    Changed the version to 2.2

---
+++ WebApiDemo/Startup.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApiDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+
+            app.UseMvc(configureRoutes =>
+            {
+                configureRoutes.MapRoute("ControllerOnlyRouteName", "api/{controller}");
+                configureRoutes.MapRoute("ControllerActionRouteName", "api/{controller}/{action}");
+            });
+        }
+    }
+}
